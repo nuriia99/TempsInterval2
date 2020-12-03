b239f4b087a4b67661ac01b92d9cc00a1aee8572
nuriia99 <RIquy212>
2020-12-03 17:42:01 +0100

    1

---
+++ src/Interval.java
@@ -0,0 +1,24 @@
+package junit.interval;
+
+public class Interval {
+    private int horaInici;
+    private int horaFi;
+
+    public void Temps(){
+
+    }
+
+    public void PeriodeDia(){
+
+    }
+
+    public void HoresMatiInterval(){
+
+    }
+
+    public void Interval(int h1, int h2){
+        horaInici = h1;
+        horaFi = h2;
+    }
+
+}
